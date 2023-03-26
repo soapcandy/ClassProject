19cbbebe5ada05ad6d66bf21040dcf217bad260f
soapcandy <octi97@naver.com>
2023-03-26 22:35:37 +0900

    일요일
    챕터 1, 2

---
+++ JavaProject/FirstJava/src/Task/Ch2/Profile.java
@@ -0,0 +1,12 @@
+package Task.Ch2;
+
+public class Profile {
+
+	public static void main(String[] args) {
+
+		System.out.println("My name is 박지훈");
+		System.out.println("My age is 25");
+
+	}
+
+}
