66e78edd97bdcb7b813cea85228ea3e76f3f528c
soapcandy <octi97@naver.com>
2023-04-10 18:21:52 +0900

    sql 초기작업
    2023-04-10 오라클db + sql 설치

---
+++ sql/sys_create_user20230410.sql
@@ -0,0 +1,11 @@
+-- 2023.04.10
+-- 주석
+
+-- 사용자 생성
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 1. 다른 사용자 -> 팝업 메뉴 -> 사용자 생성
+-- 2 계정이름 c##himedia -> c## 생략하려면
+-- ALTER SESSION SET "_ORACLE_SCRIPT"; -> 실행 -> c## 생략하고 생성가능
+-- 3. 권한 부여: connect, resource
