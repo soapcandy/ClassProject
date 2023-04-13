5a3f4417810d26c35e77f41348e990b513f45380
soapcandy <octi97@naver.com>
2023-04-13 09:57:19 +0900

    .

--- sql/select_exam03.sql
+++ sql/select_exam03.sql
@@ -50,7 +50,7 @@ select job, min(sal)from emp where mgr is not null group by job having min(sal) 
 select deptno, count(*), round(avg(sal), 2) from emp group by deptno;
 
 -- 30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
-select decode(deptno,
+select deptno, decode(deptno,
         10, 'ACCOUNTING',
         20, 'RESEARCH',
         30, 'SALE'
@@ -62,5 +62,5 @@ select decode(deptno,
         ) as "지역명",
         count(*) as "사원 수",
         round(avg(sal)) as "평균 급여"
-from emp group by deptno
+from emp group by deptno order by deptno;
         
