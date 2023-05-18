29dacba27729eaf8bf38fdf48229e04f83945cd5
soapcandy <octi97@naver.com>
2023-05-18 16:26:22 +0900

    DeptSpring

--- springProject/intelliJ/DeptSpring/src/main/java/com/hi/deptspring/deptspring/controller/rest/ExampleRestController.java
+++ springProject/intelliJ/DeptSpring/src/main/java/com/hi/deptspring/deptspring/controller/rest/ExampleRestController.java
@@ -3,6 +3,7 @@ package com.hi.deptspring.deptspring.controller.rest;
 import com.hi.deptspring.deptspring.domain.DeptDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,18 +15,23 @@ import java.util.Map;
 @RequestMapping("/test")
 public class ExampleRestController {
 
-    //메소드의 반환 타입이 String -> 문자열 응답
+    // 메소드의 반환 타입이 String -> 문자열 응답
     @GetMapping("/sample1")
     public String hello() {
-        return "Hello~!!!";
+        return "NNNNY"; // /WEB-INF/views/Hello~~!!!.jsp  X
     }
 
     @GetMapping("/sample2")
     public DeptDTO getDept() {
-        DeptDTO dept = DeptDTO.builder().deptno(90).dname("DEV").loc("서울").build();
+        DeptDTO dept = DeptDTO.builder()
+                .deptno(90)
+                .dname("DEV")
+                .loc("SEOUL")
+                .build();
 
         return dept;
     }
+
 
     @GetMapping("/sample3")
     public List<DeptDTO> getDeptList() {
@@ -36,8 +42,11 @@ public class ExampleRestController {
 
         return list;
     }
+
+
     @GetMapping("/sample4")
     public Map<Integer, DeptDTO> getDeptMap() {
+
         Map<Integer, DeptDTO> map = new HashMap<>();
         map.put(10, new DeptDTO(10, "test1", "loc1"));
         map.put(20, new DeptDTO(20, "test2", "loc2"));
@@ -45,4 +54,6 @@ public class ExampleRestController {
 
         return map;
     }
+
+
 }
